b3ff3f2652a6f72d1556b8ab9ca0139aa10c4aa1
anz213 <56008225+anzook@users.noreply.github.com>
2019-11-22 17:16:43 -0500

    working call

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,14 @@
 document.addEventListener("DOMContentLoaded", function (event) {  //waits for page load
 
     function buildQueryURL() {
-        // queryURL is the url we'll use to query the API
-        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+    //     // queryURL is the url we'll use to query the API
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?";
 
-        // Begin building an object to contain our API call's query parameters
-        // Set the API key
-        var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
+        var queryParams = { "APPID": "c04cb915be53a048550a73855778b1d9" 
+    , "units" : "imperial"};
 
         // Grab text the user typed into the search input, add to the queryParams object
-        queryParams.q = $("#search-term")
+        queryParams.q = $("#city-input")
             .val()
             .trim();
 
@@ -31,13 +30,13 @@ document.addEventListener("DOMContentLoaded", function (event) {  //waits for pa
     }
 
     // .on("click") function associated with the Search Button
-$("#run-search").on("click", function(event) {
+$("#search-btn").on("click", function(event) {
     // This line allows us to take advantage of the HTML "submit" property
     // This way we can hit enter on the keyboard and it registers the search
     // (in addition to clicks). Prevents the page from reloading on form submit.
     event.preventDefault();
   
-    // Empty the region associated with the articles
+    // Empty the search
     clear();
   
     // Build the query URL for the ajax request to the NYT API
@@ -54,8 +53,14 @@ $("#run-search").on("click", function(event) {
   //  .on("click") function associated with the clear button
   $("#clear-all").on("click", clear);
 
-  function updatePage(NYTData) {
-  
+
+/**
+ * takes API data (JSON/object) and turns it into elements on the page
+ * @param {object} WeatherData - object containing NYT API data
+ */
+  function updatePage(WeatherData) {
+  console.log(WeatherData);
+
 }
 
 });
